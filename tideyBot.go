c3a3500e00f3e75c011b651dee1e08e0728759d0
Mitchell Horne <mitchell063@hotmail.com>
2016-10-16 17:02:23 -0400

    Implemented configuration file

--- tideyBot.go
+++ tideyBot.go
@@ -4,28 +4,58 @@ import (
 	"database/sql"
 	"os"
 	"os/signal"
+	"strings"
 
 	"tideyBot/modules/plusPlus"
-	"tideyBot/modules/soundPlayer"
 
+	"github.com/BurntSushi/toml"
 	log "github.com/Sirupsen/logrus"
 	"github.com/bwmarrin/discordgo"
 	_ "github.com/mattn/go-sqlite3"
 )
+
+// Location of the main tideyBot configuration file
+const ConfigPath = "./tidey.conf"
+
+// Config is a struct to hold the configuration info
+type Config struct {
+	Token    string
+	DBPath   string
+	LogLevel string
+}
 
 func onReady(s *discordgo.Session, event *discordgo.Ready) {
 	log.Info("Recieved READY payload")
 	s.UpdateStatus(0, "fuck all y'all")
 }
 
-func main() {
+func readConfig() (*Config, error) {
+	config := new(Config)
 
-	var (
-		Token = "Bot MTcwMzI3MzkxOTA0ODU4MTEy.CglgAQ.fXXT3MkRP2B5N05pFwmByTQuUYI"
-	)
+	_, err := toml.DecodeFile(ConfigPath, &config)
+	if err != nil {
+		return config, err
+	}
+
+	log.Info("Configuration loaded successfully")
+
+	if !strings.Contains(config.Token, "Bot ") {
+		config.Token = "Bot " + config.Token
+	}
+
+	return config, nil
+}
+
+func main() {
+	// Read configuration data from file
+	config, err := readConfig()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
 	// Connect to the database
-	db, err := sql.Open("sqlite3", "./tidey.db")
+	db, err := sql.Open("sqlite3", config.DBPath)
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -34,7 +64,7 @@ func main() {
 
 	// Create a discord session
 	log.Info("Starting discord session...")
-	discord, err := discordgo.New(Token)
+	discord, err := discordgo.New(config.Token)
 	if err != nil {
 		log.WithFields(log.Fields{
 			"error": err,
@@ -58,7 +88,7 @@ func main() {
 
 	// Load Modules
 	go plusPlus.Initialize(discord, db)
-	go soundPlayer.Initialize(discord)
+	//go soundPlayer.Initialize(discord)
 
 	// Wait for a signal to quit
 	c := make(chan os.Signal, 1)
